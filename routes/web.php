728c6c072269faf2f47ba04610d905a814d24711
oscar naranjo <oscarelmono123@gmail.com>
2025-04-12 11:31:49 -0500

    vista de propietarios

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\PropietarioController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// rutas de propietarios
+Route::get('propietarios',[PropietarioController::class,'index']);
