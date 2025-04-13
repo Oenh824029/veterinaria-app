468aeed36748bc4de1e4d8007edaeabe59783215
oscar naranjo <oscarelmono123@gmail.com>
2025-04-13 12:15:36 -0500

    se agrega funcionalidad par acerrar la sesión

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -4,8 +4,9 @@
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
+    
     <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
+      <ul class="navbar-nav me-auto">
         <li class="nav-item">
           <a class="nav-link" href="{{ route('mascotas.index') }}">Mascotas</a>
         </li>
@@ -14,6 +15,18 @@
         </li>
         <li class="nav-item">
           <a class="nav-link" href="{{ route('propietarios.index') }}">Propietarios</a>
+        </li>
+      </ul>
+
+      <!-- Botón de Cerrar Sesión -->
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <form method="POST" action="{{ route('logout') }}">
+            @csrf
+            <button type="submit" class="nav-link btn btn-link text-white text-decoration-none" style="padding: 0; border: none; background: none;">
+              Cerrar sesión
+            </button>
+          </form>
         </li>
       </ul>
     </div>
